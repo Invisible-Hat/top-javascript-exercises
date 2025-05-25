1db6d0af8ab0b9808ee4471daba5bb07b5daa2c1
Daniele Renzetti <limewaster@gmail.com>
2025-05-25 19:43:35 +0200

    Completed reverseStrings, all tests passed: solved

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let array = "";
+  for (let i = word.length; i >= 0; i--) {
+    array += word.charAt(i);
+  }
+  return array;
 };
 
 // Do not edit below this line
