d57aa42f93c3d1e1f83b528454416dc760e51300
Daniele Renzetti <limewaster@gmail.com>
2025-06-03 18:31:38 +0200

    #09 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) return "ERROR";
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  let arr = [];
+  const max = a > b ? a : b;
+  const min = a > b ? b : a;
+  for (let i = min; i <= max; i++) {
+    arr.push(i);
+  }
+  return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
 // Do not edit below this line
