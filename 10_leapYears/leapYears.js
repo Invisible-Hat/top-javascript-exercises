ecb3a7cbb575ba3888b2eaad3ca4899feb34fce6
Daniele Renzetti <limewaster@gmail.com>
2025-06-04 13:03:51 +0200

    #10 done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let boolean = false;
+  if (year % 4 === 0 && year % 100 !== 0) boolean = true;
+  if (year % 100 === 0 && year % 400 === 0) boolean = true;
+  return boolean;
 };
 
 // Do not edit below this line
